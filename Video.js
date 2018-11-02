f9adc1baff0b41afeab1aa441915b8e7e2ac183b
苗帅 <miaoshuait@163.com>
2018-11-02 13:34:13 +0800

    增加poster的宽度高度属性设置posterWidth、posterHeight

--- Video.js
+++ Video.js
@@ -263,6 +263,8 @@ export default class Video extends Component {
         top: 0,
         right: 0,
         bottom: 0,
+		width:this.props.posterWidth,
+		height:this.props.posterHeight
         resizeMode: this.props.posterResizeMode || 'contain'
       };
 
@@ -321,6 +323,8 @@ Video.propTypes = {
   ]),
   resizeMode: PropTypes.string,
   poster: PropTypes.string,
+  posterWidth:PropTypes.number,
+  posterHeight:PropTypes.number,
   posterResizeMode: Image.propTypes.resizeMode,
   repeat: PropTypes.bool,
   allowsExternalPlayback: PropTypes.bool,
